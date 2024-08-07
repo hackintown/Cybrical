5fee268455a7886e034b9ba9aa0d41dfcd894abf
manoj mandal <hackintown99@gmail.com>
2024-08-07 16:42:32 +0530

    Navbar-Added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,110 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Button from "./ui/Button";
+
+const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="bg-white border-b shadow-md">
+      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+        <div>
+          <Image
+            src="/images/logo.png"
+            height={100}
+            width={100}
+            alt="Logo"
+            className="h-8 mr-3"
+          />
+        </div>
+        <div className="hidden md:flex space-x-6">
+          <Link href="/services" className="text-gray-600 hover:text-green-500">
+            Services
+          </Link>
+          <Link
+            href="/technologies"
+            className="text-gray-600 hover:text-green-500"
+          >
+            Technologies
+          </Link>
+          <Link href="/work" className="text-gray-600 hover:text-green-500">
+            Work
+          </Link>
+          <Link href="/company" className="text-gray-600 hover:text-green-500">
+            Company
+          </Link>
+          <Link
+            href="/hire-developer"
+            className="text-gray-600 hover:text-green-500"
+          >
+            Hire Developer
+          </Link>
+        </div>
+        <div className="hidden md:block">
+          <Button>Start a Project</Button>
+        </div>
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-gray-600 hover:text-green-500 focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="md:hidden">
+          <Link
+            href="/services"
+            className="block px-4 py-2 text-gray-600 hover:bg-gray-100"
+          >
+            Services
+          </Link>
+          <Link
+            href="/technologies"
+            className="block px-4 py-2 text-gray-600 hover:bg-gray-100"
+          >
+            Technologies
+          </Link>
+          <Link
+            href="/work"
+            className="block px-4 py-2 text-gray-600 hover:bg-gray-100"
+          >
+            Work
+          </Link>
+          <Link
+            href="/company"
+            className="block px-4 py-2 text-gray-600 hover:bg-gray-100"
+          >
+            Company
+          </Link>
+          <Link
+            href="/hire-developer"
+            className="block px-4 py-2 text-gray-600 hover:bg-gray-100"
+          >
+            Hire Developer
+          </Link>
+          <div className="px-4 py-2">
+            <Button>Start a Project</Button>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1140px",
+        "2xl": "1200px",
       },
     },
     extend: {
@@ -70,19 +70,8 @@ const config: Config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+      backgroundImage: {
+        "gradient-card": "linear-gradient(180deg, #0EA301 0%, #003C06 83%)",
       },
     },
   },
