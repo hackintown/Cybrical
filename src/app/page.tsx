a84de165fc25958d380211d729c73527d1872907
manoj mandal <hackintown99@gmail.com>
2024-08-07 18:13:33 +0530

    project-config-v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
-
+import Hero from "../components/Home/Hero";
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 }
